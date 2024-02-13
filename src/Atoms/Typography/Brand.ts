9ee8aba371395efa16de37c3f1e02f15792bfae7
HenrryEsposito <henrrygesposito@gmail.com>
2024-02-13 09:26:31 -0300

    feat - detalhes de tipografia

---
+++ src/Atoms/Typography/Brand.ts
@@ -0,0 +1,24 @@
+export const Headlines = {
+    h1: {
+        fontSize: '3rem',
+        fontWeight: 'bold',
+    },
+    h2: {
+        fontSize: '2.25rem',
+        fontWeight: 'bold',
+    },
+    h3: {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+    },
+};
+
+export const BodyText = {
+    fontSize: '1rem',
+    fontWeight: 'regular',
+};
+
+export const CallToAction = {
+    fontSize: '1.125rem',
+    fontWeight: 'regular',
+};
---
+++ src/Atoms/Typography/Interface.ts
@@ -0,0 +1,24 @@
+export const BodySmall = {
+    fontSize: '0.75rem',
+    fontWeight: 'regular',
+};
+
+export const TitleSmall = {
+    fontSize: '0.875rem',
+    fontWeight: 'regular',
+};
+
+export const Body = {
+    fontSize: '1rem',
+    fontWeight: 'bold',
+};
+
+export const Title = {
+    fontSize: '1.125rem',
+    fontWeight: 'medium',
+};
+
+export const TitleLarge = {
+    fontSize: '1.5rem',
+    fontWeight: 'regular',
+};
